fb298a66437e3c0fb415770607f168555c5fcdcb
MahfuzAhmed28 <mahfuzyasin28@gmail.com>
2024-10-04 20:58:39 +0600

    last_change

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,75 @@ class HelloWorld extends StatelessWidget{
         //color: Colors.red,
         //color: Colors.red.shade50,
         alignment: Alignment.bottomCenter,),*/
-        body: Image.network('https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg',
+
+        /*body: Image.network('https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg',
         width: 200,
         height: 400,
-        fit: BoxFit.cover,),
+        fit: BoxFit.cover,),*/
+
+        /*body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Hello'),
+            Text('World'),
+            Image.network('https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg',
+            width: 200,
+            height: 300,
+            fit: BoxFit.cover,),
+            Text('This is Shoe pic')
+          ],
+        ),*/
+        /*body: Row(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,  4 kore
+          mainAxisAlignment: MainAxisAlignment.spaceAround,  // 2 kore sob pase
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,  //mainAxisAlignment k override kore
+          children: [
+            Text('Hello'),
+            Text('Another'),
+            Text('World'),
+            Image.network('https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg',
+              width: 100,),
+            Text('Shoe'),
+          ],
+        )*/
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello World'),
+            Row(
+              children: [
+                Text('Hello'),
+                Text('World'),
+                Image.network('https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg',
+                  width: 100,
+                  fit: BoxFit.cover,),
+                Text('Shoe'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.network('https://t3.ftcdn.net/jpg/06/12/00/18/360_F_612001823_TkzT0xmIgagoDCyQ0yuJYEGu8j6VNVYT.jpg',
+                  width: 100,
+                  fit: BoxFit.cover,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('This'),
+                    Text('is'),
+                    Text('Nike new Version'),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
       //title: ='Welcome to Flutter',
     );
