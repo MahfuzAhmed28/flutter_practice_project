cd0503eec197142013a1440318cce71704784125
MahfuzAhmed28 <mahfuzyasin28@gmail.com>
2024-10-13 15:13:02 +0600

    Dialoge_modalSheet_TextInput

--- lib/main.dart
+++ lib/main.dart
@@ -30,130 +30,137 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(
               height: 10,
             ),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                foregroundColor: Colors.amberAccent,
-                textStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900,
-                ),
-                //padding: EdgeInsets.all(100),
-                //padding: EdgeInsets.only(top: 10,left: 10),
-                padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
-                shadowColor: Colors.green,
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-
-                ),
-                side: BorderSide(
-                  color: Colors.green,
-                  width: 5,
-                ),
-                minimumSize: Size(100, 50),
-                //maximumSize: Size(200, 200)
-              ),
-              onPressed: (){
-                //print('Tap on Elaveted Button');
-                showDialog(
-                  barrierColor: Colors.green,
-                  barrierDismissible: false,
+              onPressed: () {
+                //showAboutDialog(context: context);
+                showModalBottomSheet(
+                  //barrierColor: Colors.red,
+                  //backgroundColor: Colors.green.shade200,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  isScrollControlled: true,
+                  useSafeArea: true,
+                  enableDrag: false,
                   context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Delete'),
-                      content: Text('Are You Sure?'),
-                      actions: [
-                        TextButton(onPressed: () {}, child: Text('Yes')),
-                        TextButton(onPressed: () {}, child: Text('No')),
+                  builder: (ctx){
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                  'Title',
+                                  style: TextStyle(
+                                    fontSize: 20
+                                  ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          height: 20,
+                          thickness: 5,
+                          color: Colors.green,
+                        ),
+                        Text('Sample'),
+                        Row(
+                          children: [
+                            ElevatedButton(onPressed: () {}, child: Text('Save')),
+                            ElevatedButton(onPressed: () {}, child: Text('Cancel')),
+                          ],
+                        )
+                        //Divider(),
+                        //Text('Yes'),
                       ],
                     );
-                });
+                  }
+                );
               },
-              child: Text('Tap'),
+              child: Text('Show dialoge'),
             ),
             SizedBox(
               height: 10,
             ),
-            TextButton(
-              style: TextButton.styleFrom(),
-                onPressed: () {
-                  print('Text Button Pressed');
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                maxLength: 50,
+                onChanged: (String? value)
+                {
+                  print(value);
                 },
-                child: Text('Click Here'),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            IconButton(
-              style: IconButton.styleFrom(),
-              onPressed: () {
-                int a=10;
-                print('Icon Button Pressed ${a}');
-              },
-              icon: Icon(Icons.add),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(),
-              onPressed: () {
-                int a=10;
-                print('Icon Button Pressed ${a}');
-              },
-              //child: Icon(Icons.abc),
-              child: Text('Press'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              onTap: () {
-                print("Just On Tap");
-              },
-              onDoubleTap: () {
-                print('Double Tap');
-              },
-              onLongPress: () {
-                print('Long Press');
-              },
-              onLongPressCancel: () {
-                print('Long Press Cancel');
-              },
-              onLongPressEnd: (details) {
-                print('Long Press End');
-              },
-              child: Column(
-                children: [
-                  Text('Simple Button'),
-                  Text('Simple Button'),
-                  Text('Simple Button'),
-                  Text('Simple Button'),
-                ],
+                controller: TextEditingController(),
+                //obscureText: true,
+                maxLines: 1,
+                //keyboardType: TextInputType.phone,
+                enabled: true,
+                decoration: InputDecoration(
+                  hintText: 'Phone',
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.w100,
+                  ),
+                  labelText: 'Phone Number',
+                  //label: Icon(Icons.add),
+                  prefixIcon: Icon(Icons.phone),
+                  suffixIcon: Icon(Icons.person),
+                  // prefix: Column(
+                  //   children: [
+                  //     Text('abcd'),
+                  //     Text('edgh'),
+                  //   ],
+                  // ),
+                  // suffix: Column(
+                  //   children: [
+                  //     Text('abcd'),
+                  //     Text('edgh'),
+                  //   ],
+                  // ),
+                  icon: Icon(Icons.ac_unit),
+                  //counterText: '',
+                  counterStyle: TextStyle(
+                    fontSize: 20
+                  ),
+                  fillColor: Colors.pink,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    )
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 3
+                    )
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue
+                    )
+                  )
+
+                ),
               ),
             ),
-            InkWell(
-              splashColor: Colors.green,
-            onTap: () {
-              print('Ink Well');
-            },
-            child: Text('Behave Like Button')
-            ),
+
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-    
-        },
-        child: Icon(Icons.add),
-      ),
+
     );
   }
 }
