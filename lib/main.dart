77e3f23f41a3a93f2a2c88dc8ed953cb43ab6f63
MahfuzAhmed28 <mahfuzyasin28@gmail.com>
2024-11-09 15:50:59 +0600

    NamedRoute and statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,149 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(CupertinoExperiment());
+void main()
+{
+  runApp(SimpleApp());
 }
-
-class CupertinoExperiment extends StatelessWidget {
-  const CupertinoExperiment({super.key});
+class SimpleApp extends StatelessWidget {
+  const SimpleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Experiment',
-      home: Home(),
+      /*initialRoute: '/home',
+      routes: {
+        '/home': (context) =>Home(),
+        '/profile': (context) =>Profile(),
+        '/setting': (context) =>Setting()
+      },*/
+      home: CounterScreen(),
     );
   }
 }
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Hello'),
-        trailing: Icon(CupertinoIcons.add),
-        leading: Icon(CupertinoIcons.airplane),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
         children: [
-          CupertinoButton.filled(
-            borderRadius: BorderRadius.circular(18),
-            child: Text('Tap Here'), onPressed: () {}),
-          CupertinoListTile(
-            onTap: () {},
-            title: Text('List'),
-            backgroundColor: CupertinoColors.systemPurple,
-            subtitle: Text('Text'),
-            trailing: Icon(CupertinoIcons.airplane),
-          ),
-          CupertinoTextField(),
-          CupertinoSwitch(value: false, onChanged: (value) {})
+          ElevatedButton(
+            onPressed: () {
+              /*Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Profile()));*/
+              //Navigator.pushNamed(context, '/profile');
+              Navigator.pushReplacementNamed(context, '/profile');
+              },
+
+            child: Text('Goto Profile'))
         ],
       ),
     );
   }
 }
 
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              /*Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Setting()));*/
+              Navigator.pushReplacementNamed(context, '/setting');
+            },
+            child: Text('Settings'))
+        ],
+      ),
+    );
+  }
+}
+
+
+class Setting extends StatelessWidget {
+  const Setting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Setting'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              //Navigator.pushNamed(context, '/home');
+              Navigator.pushNamedAndRemoveUntil(context, '/home', (predicate)=>false);
+            },
+            child: Text('Home'))
+        ],
+      ),
+    );
+  }
+}
+
+/*class CounterScreen extends StatelessWidget {
+  CounterScreen({super.key});
+  int counter=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+          child: Text('Counter Value is $counter'),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {counter++;},child: Icon(Icons.add),),
+    );
+  }
+}*/
+
+
+class CounterScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return CounterScreenState();
+  }
+}
+
+class CounterScreenState extends State<CounterScreen>
+{
+  int counter=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Text('Counter Value is $counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter=counter+1;
+          print(counter);
+          setState(() {});
+        },
+        child: const Icon(Icons.add),),//cost use korle seti r rebuild hobe na
+    );
+  }
+
+}
 
 
