e6a846c78cc21635abe4fa212e3fe162d471f70d
MahfuzAhmed28 <mahfuzyasin28@gmail.com>
2024-11-09 14:42:09 +0600

    Cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HelloWorld());
+  runApp(CupertinoExperiment());
 }
 
-class HelloWorld extends StatelessWidget {
+class CupertinoExperiment extends StatelessWidget {
+  const CupertinoExperiment({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //Remove Corner Debug Banner
-      title: 'Welcome',
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Experiment',
       home: Home(),
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)
-              )
-          ),
-        )
-      ),
     );
   }
 }
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Hello'),
+        trailing: Icon(CupertinoIcons.add),
+        leading: Icon(CupertinoIcons.airplane),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Profile(username: 'Mahfuz');
-                  }));
-                },
-              child: Text('Goto Pfofile')
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return Settings();
-                  }));
-                },
-                child: Text('Settings')
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CupertinoButton.filled(
+            borderRadius: BorderRadius.circular(18),
+            child: Text('Tap Here'), onPressed: () {}),
+          CupertinoListTile(
+            onTap: () {},
+            title: Text('List'),
+            backgroundColor: CupertinoColors.systemPurple,
+            subtitle: Text('Text'),
+            trailing: Icon(CupertinoIcons.airplane),
+          ),
+          CupertinoTextField(),
+          CupertinoSwitch(value: false, onChanged: (value) {})
+        ],
       ),
     );
   }
 }
 
-class Profile extends StatelessWidget {
-  const Profile({super.key,required this.username});
-  final String username;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(username),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Back')
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Settings extends StatelessWidget {
-  const Settings({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            ElevatedButton(
-
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Back')
-            ),
-            ElevatedButton(onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Profile(username: 'Rakib',)));
-              },
-              child: Text('Move to Profil')
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 
